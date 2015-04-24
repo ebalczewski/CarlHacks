87ba1b31be2c2ce7b39c112cd2b55ecd3ddff525
ebalczewski <balczewskie@carleton.edu>
2015-04-23 22:15:57 -0500

    Initial Commit

---
+++ Tabinet/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Tabinet
+//
+//  Created by Emily Balczewski on 4/23/15.
+//  Copyright (c) 2015 ebal. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
