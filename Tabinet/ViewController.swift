19b05bddf7c8e67c1919491f617d10c939df7548
Sean Mullan <seanmm@mchsi.com>
2015-04-24 22:43:14 -0500

    table added

--- Tabinet/ViewController.swift
+++ Tabinet/ViewController.swift
@@ -8,11 +8,12 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
-
+class ViewController: NSViewController, NSTableViewDataSource, NSTableViewDelegate{
+    @IBOutlet weak var tableView: NSTableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        tableView.setDelegate(self)
+        tableView.setDataSource(self)
         // Do any additional setup after loading the view.
     }
 
@@ -21,7 +22,28 @@ class ViewController: NSViewController {
         // Update the view, if already loaded.
         }
     }
-
+    
+    //MARK: Table Stuff
+    
+    func tableView(tableView: NSTableView, viewForTableColumn tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        var viewCell : NSTableCellView = NSTableCellView(frame: NSMakeRect(0,0,tableView.bounds.size.width,tableView.rowHeight))
+        var text : NSTextView = NSTextView(frame: NSMakeRect(0, 0, tableColumn!.width, viewCell.bounds.size.height))
+        viewCell.addSubview(text)
+        text.string = "Hello"
+        text.editable = false
+        
+        var dropShadow = NSShadow()
+        dropShadow.shadowColor = NSColor.redColor()
+        dropShadow.shadowOffset = NSMakeSize(0,-10)
+        dropShadow.shadowBlurRadius = 10
+        viewCell.wantsLayer = true
+        viewCell.shadow = dropShadow
+        
+        return viewCell
+    }
+    func numberOfRowsInTableView(tableView: NSTableView) -> Int {
+        return 3
+    }
 
 }
 
